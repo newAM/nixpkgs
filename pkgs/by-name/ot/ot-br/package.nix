8fd2e3cac42890d4cd489e04c6403089b7de40c4
Alex Martens <alex@thinglab.org>
2024-06-02 12:27:56 -0700

    with web

--- pkgs/by-name/ot/ot-br/package.nix
+++ pkgs/by-name/ot/ot-br/package.nix
@@ -10,7 +10,9 @@
 # web
 , jsoncpp
 , boost
-, nodejs_20
+, nodejs
+, fetchNpmDeps
+, buildNpmPackage
 }:
 
 let
@@ -20,7 +22,7 @@ in
 # TO RUN: https://github.com/home-assistant/addons/blob/d8e2216ef532e21948678720140a45a3b4fa6f3f/openthread_border_router/rootfs/etc/s6-overlay/s6-rc.d/otbr-agent/run#L120
 # backbone will be "br0"
 # result/bin/otbr-agent -I $THREAD_NET_IF -B $BACKBONE_NET_IF --rest-listen-address "::" -d7 -v spinel+hdlc+uart:///dev/serial/by-id/usb-Nabu_Casa_SkyConnect_v1.0_3c05fd8faf9ced1181a877faa7669f5d-if00-port0?uart-baudrate=460800 trel://$BACKBONE_NET_IF
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "ot-br";
   version = "unstable-2024-05-28";
 
@@ -31,6 +33,23 @@ stdenv.mkDerivation {
     rev = "2279c02f3c3373f074899fc8d993b8ddb72910a2";
     fetchSubmodules = true;
     hash = "sha256-MsQRue7gg5U0n/nbIYzSMNVB17iIFeGfc3sl6LcKI9U=";
+  };
+
+  nodeModules = buildNpmPackage rec {
+    inherit pname version src;
+
+    dontNpmBuild = true;
+
+    npmRoot = "src/web/web-service/frontend";
+
+    npmDeps = fetchNpmDeps {
+      hash = "sha256-7UVfPICyIbHEClpr3p7eDR46OUzS8mVf6P7phnDpVLk=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/${pname}
+      cp -r ${npmRoot}/node_modules/ $out/share/${pname}
+    '';
   };
 
   patches = [
@@ -63,17 +82,19 @@ stdenv.mkDerivation {
       git -C third_party/openthread/repo apply ${sumodulePatch}
 
       substituteInPlace src/web/CMakeLists.txt \
-        --replace "Boost_USE_STATIC_LIBS ON" "Boost_USE_STATIC_LIBS OFF" \
-        --replace "add_dependencies(otbr-web otbr-web-frontend)" "" \
-        --replace "add_subdirectory(web-service/frontend)" ""
+        --replace-fail "Boost_USE_STATIC_LIBS ON" "Boost_USE_STATIC_LIBS OFF"
+
+      substituteInPlace src/web/web-service/frontend/CMakeLists.txt \
+        --replace-fail "npm install" "echo nop"
+
+      substituteInPlace src/web/web-service/frontend/CMakeLists.txt \
+        --replace-fail ' ''${CMAKE_CURRENT_BINARY_DIR}' " ${nodeModules}/share/${pname}"
     '';
 
   nativeBuildInputs = [
     cmake
     pkg-config
     gitMinimal
-    # web
-    # nodejs_20
   ];
 
   buildInputs = [
@@ -83,7 +104,6 @@ stdenv.mkDerivation {
     jsoncpp
     boost
   ];
-  # systemdLibs maybe for web
 
   # https://github.com/home-assistant/addons/blob/d8e2216ef532e21948678720140a45a3b4fa6f3f/openthread_border_router/Dockerfile#L67
   cmakeFlags = [
